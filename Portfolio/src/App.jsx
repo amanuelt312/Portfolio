b189d881228bc8a9365cedf5e618035b7e7e1e4f
Amanuel Tesfaye <amanuelt312@gmail.com>
2024-07-31 13:46:10 +0300

    Update App remove animation.jsx

--- Portfolio/src/App.jsx
+++ Portfolio/src/App.jsx
@@ -12,33 +12,7 @@ function App() {
 
   return (
     <BrowserRouter>
-      <AnimatedCursor
-        innerSize={15}
-        outerSize={8}
-        color="111, 66, 201"
-        outerAlpha={0.2}
-        innerScale={0.7}
-        outerStyle={{
-          border: "3px solid rgb(111, 66, 201)",
-        }}
-        innerStyle={{
-          backgroundColor: "rgb(111, 66, 201)",
-        }}
-        outerScale={5}
-        clickables={[
-          "a",
-          'input[type="text"]',
-          'input[type="email"]',
-          'input[type="number"]',
-          'input[type="submit"]',
-          'input[type="image"]',
-          "label[for]",
-          "select",
-          "textarea",
-          "button",
-          ".link",
-        ]}
-      />
+      
       <Background></Background>
       <div className="content">
         <Navbar />
